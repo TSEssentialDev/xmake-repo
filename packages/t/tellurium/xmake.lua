13d10bc6234632583b693244f23f49a3bae1c5a6
xianyubb <2149656630@qq.com>
2024-08-27 18:52:32 +0800

    Change project name as Tellurium

--- packages/t/tellurium/xmake.lua
+++ packages/t/tellurium/xmake.lua
@@ -1,6 +1,6 @@
 package("tsessential")
-    add_urls("https://github.com/TSEssentialDev/TSEssential/releases/download/v$(version)/SDK.zip")
-
+    add_urls("https://github.com/TelluriumDev/Tellurium/releases/download/v$(version)/SDK.zip")
+    
     on_install(function (package)
         os.cp("include", package:installdir())
         os.cp("lib/*.lib", package:installdir("lib"))
