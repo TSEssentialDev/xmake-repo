6dd4211a234ea62ac752725ab53e851a2d9fa210
xianyubb <2149656630@qq.com>
2024-09-28 14:51:32 +0800

    Add package scriptx with configurations for backend and install/load rules

---
+++ packages/s/scriptx/xmake.lua
@@ -0,0 +1,30 @@
+package("scriptx")
+    add_urls("https://github.com/TelluriumDev/ScriptX.git")
+    add_versions("1.0.0", "abe9bc4c0b83727bf04c1af838951c05226fb65f")
+
+    add_configs("backend", {default = "Lua", values = {"Lua", "quickjs"}})
+    add_includedirs(
+        "include/src/include/"
+    )
+
+    on_install(function (package)
+        local configs = {
+            backend = package:config("backend"),
+        }
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_load(function (package)
+        local backend = package:config("backend")
+
+        local deps = {
+            Lua = "lua",
+            quickjs = "quickjs",
+        }
+
+        print("Using ScriptX config: backend=" .. backend)
+    
+        package:add("defines", "SCRIPTX_BACKEND=" .. backend)
+        package:add("defines", "SCRIPTX_BACKEND_TRAIT_PREFIX=../backend/" .. backend .. "/trait/Trait")
+        package:add("deps", deps[backend])
+    end)
